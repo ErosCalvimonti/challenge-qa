1f2bdcfc34cf0ee6d0b2f8938c05e149d74728cc
Eros Calvimonti <karielcode@gmail.com>
2024-10-14 17:30:27 -0300

    Se agrega el cuarto test case

--- cypress/e2e/pages/calendarPage.js
+++ cypress/e2e/pages/calendarPage.js
@@ -24,7 +24,7 @@ export class CalendarPage extends BasePage {
     this.clickElement("div[class='Grid_header__yAoy_'] div:nth-child(3)");
   }
 
-  isFebruary() {
+  goToFebruary() {
     this.getElement("div[class*='Grid_header_'] div:nth-child(2)")
       .should("exist")
       .then(($monthTitle) => {
@@ -35,7 +35,7 @@ export class CalendarPage extends BasePage {
           this.goToNextMonth();
 
           // Llama de nuevo a isFebruary para verificar si ya es febrero
-          this.isFebruary();
+          this.goToFebruary();
         } else {
           cy.log("It's February");
         }
@@ -45,7 +45,7 @@ export class CalendarPage extends BasePage {
   // Navega hasta febrero y asegura que el evento del Día de la Marmota esté presente
   navigateAndEnsureGroundhogDayEvent() {
     // Usa isFebruary para navegar hasta febrero
-    this.isFebruary();
+    this.goToFebruary();
 
     // Luego, verifica y asegura el evento
     this.ensureGroundhogDayEvent();
@@ -53,9 +53,8 @@ export class CalendarPage extends BasePage {
 
   // Verifica si el evento "Groundhog Day" está presente mediante el texto, o lo agrega si no está
 ensureGroundhogDayEvent() {
-    cy.get(".Grid_spaceHolder__IVVTT > :nth-child(6)")
+    this.getElement(".Grid_spaceHolder__IVVTT > :nth-child(6)")
       .should('have.text', '2') // Selecciona el día 2 de febrero
-      .closest("div") // Encuentra el contenedor del día
       .then(($dayContainer) => {
         // Verifica si el texto "Groundhog Day" está presente dentro del contenedor
         if ($dayContainer.text().includes("Groundhog Day")) {
@@ -68,10 +67,23 @@ ensureGroundhogDayEvent() {
     // Método para agregar el evento del Día de la Marmota en el día 2 de febrero
   addGroundhogDayEvent() {
     // Selecciona el día 2 de febrero
-    cy.get(".Grid_spaceHolder__IVVTT > :nth-child(6)").contains("2").click(); 
+    this.getElement(".Grid_spaceHolder__IVVTT > :nth-child(6)").contains("2").click(); 
     // Ingresa el nombre del evento
-    cy.get("textarea").type("Groundhog Day"); 
+    this.getElement("textarea").type("Groundhog Day"); 
     // Guarda el evento
-    cy.get(".Grid_icon__bYeIv > .Grid_customIcon__JyTa0").click(); 
+    this.getElement(".Grid_icon__bYeIv > .Grid_customIcon__JyTa0").click(); 
+  }
+  // Método para eliminar el evento del Día de la Marmota si está presente
+  deleteGroundhogDayEvent() {
+    this.getElement(".Grid_spaceHolder__IVVTT > :nth-child(6)").contains("2").click(); 
+    this.getElement(":nth-child(1) > .Grid_reminderContent__pLoKD > .Grid_reminderIcons__4mm3s > .Grid_icon-cancel__HmIzw").click()
+    this.getElement(".Grid_icon__bYeIv > .Grid_customIcon__JyTa0").click()
+
+  }
+
+  // Método para verificar que el evento ya no está presente
+  isGroundhogDayEventDeleted() {
+    return this.getElement("Grid_reminder__OelsH")
+    .should('not.exist');
   }
 }
--- cypress/e2e/test/calendarTests.cy.js
+++ cypress/e2e/test/calendarTests.cy.js
@@ -1,24 +1,34 @@
-import { CalendarPage } from '../pages/CalendarPage';
+import { CalendarPage } from "../pages/CalendarPage";
 const calendarPage = new CalendarPage();
-describe('Challenge Cypress Minimalart', () => {
-    
-  beforeEach('Navigate to calendar page', () => {
-    calendarPage.openCalendar()
+describe("Challenge Cypress Minimalart", () => {
+  beforeEach("Navigate to calendar page", () => {
+    calendarPage.openCalendar();
   });
 
-  it('Should navigate to the calendar page correctly', () => {
-    calendarPage.checkPageTitle()
-    calendarPage.checkUrlCallendar()
+  it("Should navigate to the calendar page correctly", () => {
+    calendarPage.checkPageTitle();
+    calendarPage.checkUrlCallendar();
   });
 
-//   Iterar hasta que el mes actual sea febrero
-  it('Should navigate to February', () => {
-    calendarPage.isFebruary()
+  //   Iterar hasta que el mes actual sea febrero
+  it("Should navigate to February", () => {
+    calendarPage.goToFebruary();
   });
 
-  it('Should navigate to February and ensure Groundhog Day event is present if not added', () => {
-    calendarPage.isFebruary();
+  it("Should navigate to February and ensure Groundhog Day event is present if not added", () => {
+    calendarPage.goToFebruary();
     calendarPage.ensureGroundhogDayEvent();
-  })
+  });
 
-})
+  // Test case para borrar el evento del Día de la Marmota
+  it("Should delete Groundhog Day event if present", () => {
+    // Asegura que esté en febrero
+    calendarPage.goToFebruary();
+    // Asegura que el evento esté presente para poder borrarlo
+    calendarPage.ensureGroundhogDayEvent();
+    // Elimina el evento
+    calendarPage.deleteGroundhogDayEvent();
+    // Verifica que el evento haya sido eliminado
+    calendarPage.isGroundhogDayEventDeleted();
+  });
+});
